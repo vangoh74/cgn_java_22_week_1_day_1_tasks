b0425482d47f02f5ecc845173087ee5b41c89ba7
Vandicleia Goncalves <vangoh05@protonmail.com>
2022-03-24 01:02:08 +0100

    Update and create function to Display intermediate result horizontally or vertically

--- src/hangman_game/HangmanGame.java
+++ src/hangman_game/HangmanGame.java
@@ -12,9 +12,9 @@ import java.util.Scanner;
 * */
 public class HangmanGame {
     // Globale Variabel
-    public static final String WORD = "Schilddrüse";
+    public static final String WORD = "ola";
     public static int iterator = 0;
-    public static int elements;
+    public static int descendingErrorCounter;
     public static int choices;
     public static String allLetters = "";
     public static char[] present = new char[WORD.length()];
@@ -25,14 +25,14 @@ public class HangmanGame {
         String str = deleteDup();
 
         //Wort ohne Duplikate
-        elements = str.length();
+        descendingErrorCounter = str.length();
         choices = str.length();
 
         // Speichert die Usereingaben ohne wiederholung
         char[] letters = new char[str.length()];
         char letter = ' ';
         System.out.println("\nHANGMAN GAME: Versuche das Wort zu raten!!\n\nAUFPASSEN!!\nBei " + str.length() + " falsche Angaben hast Du Verloren!!!\nDas Gesuchte Wort hat insgesamt " + WORD.length() + " Buchstaben!\nLOS GEHT'S!!\n");
-        while (elements > 0) {
+        while (descendingErrorCounter > 0) {
             letter = getUserInput(letters);
         }
         getGameResult(letters, str);
@@ -54,20 +54,21 @@ public class HangmanGame {
             } else if (proofUserInput(letter)) {
                 setUserInputToCorrectPosition(letter);
                 System.out.println("TREFFER!!");
-                printCurrentStatus();
+                //printCurrentStatusVertical();
+                printCurrentStatusHorizontal();
                 letters[iterator] = letter;
                 allLetters += input;
                 ++iterator;
                 if (WORD.toLowerCase().equals(changeArrayToString(present))) {
-                    elements = 0;
+                    descendingErrorCounter = 0;
                     return letter;
                 }
 
             } else {
                 allLetters += input;
-                --elements;
-                System.out.println("FALSCH! Du hast " + (choices - elements) + " Fehler!");
-                if (elements == 0) {
+                --descendingErrorCounter;
+                System.out.println("FALSCH! Du hast " + (choices - descendingErrorCounter) + " Fehler!");
+                if (descendingErrorCounter == 0) {
                     return letter;
                 }
             }
@@ -80,19 +81,39 @@ public class HangmanGame {
         return letter;
     }
 
+    public static void printCurrentStatusHorizontal() {
+        System.out.println("Zwischenstand: ");
+
+        int arrLength = (present.length * 2) - 1 ;
+        char[] arr = new char[arrLength];
+        char empty = ' ';
+        int counter = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            // Leerstelle zw. Buchstaben einfügen: abcdef --> a b c d e f
+            if ((i % 2 == 0) || (i == arr.length-1)) {
+                arr[i] = present[i - counter];
+            } else {
+                arr[i] = empty;
+                counter++;
+            }
+        }
+        String correktUserInputAsString = changeArrayToString(arr);
+        System.out.println(correktUserInputAsString);
+    }
+
+    public static void printCurrentStatusVertical() {
+        System.out.println("Zwischenstand: ");
+        for (char c : present) {
+            System.out.println(c);
+        }
+    }
     public static String changeArrayToString(char[] arr) {
         String str = "";
         for (int i = 0; i < arr.length; i++) {
             str = str + arr[i];
         }
         return str;
-    }
-
-    public static void printCurrentStatus() {
-        System.out.println("Zwischenstand: ");
-        for (char c : present) {
-            System.out.println(c);
-        }
     }
 
     // Prüft ob Buchstabe in Array vorkommt
@@ -155,9 +176,9 @@ public class HangmanGame {
                 }
             }
             if (backStr.length() == str.length()) {
-                System.out.println("!!! GLÜCKWUNSCH !!!\nDU HAST GEWONNEN! :-))\n");
+                System.out.println("\n!!! GLÜCKWUNSCH !!!\nDU HAST GEWONNEN! :-))\n");
             } else {
-                System.out.println("\nPECH!! DU HAST VERLOREN! :-(\nVIELLEICHT EIN NÄCHSTES MAL!!");
+                System.out.println("\nPECH GEHABT!! DU HAST VERLOREN! :-(\nVIELLEICHT EIN NÄCHSTES MAL!!");
             }
         }
     }
